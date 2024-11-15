44bf0588c9082e0f12cbd420de055e4c56a52c60
Kardygan <k4rdygan@gmail.com>
2024-11-15 01:46:39 +0100

    Add AlbumDTO & TrackDTO

---
+++ server/PlayItLoud.API/Models/DTOs/AlbumDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace PlayItLoud.API.Models.DTOs
+{
+    public class AlbumDTO
+    {
+        public string Name { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public string? Description { get; set; }
+        public string? Label { get; set; }
+        public ICollection<int> ArtistIds { get; set; } = new List<int>();
+        public ICollection<int> GenreIds { get; set; } = new List<int>();
+        public ICollection<TrackDTO> Tracks { get; set; } = new List<TrackDTO>();
+    }
+}
---
+++ server/PlayItLoud.API/Models/DTOs/TrackDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayItLoud.API.Models.DTOs
+{
+    public class TrackDTO
+    {
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
