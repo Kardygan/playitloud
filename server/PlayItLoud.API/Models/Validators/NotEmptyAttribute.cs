25cbd485bb448b9619452451138ba0818457cf10
Kardygan <k4rdygan@gmail.com>
2024-11-17 17:28:49 +0100

    Add custom ValidationAttributes

---
+++ server/PlayItLoud.API/Models/Validators/NotEmptyAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlayItLoud.API.Models.Validators
+{
+    public class NotEmptyAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is ICollection collection && collection.Count > 0)
+            {
+                
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage ?? "The collection must contain at least one element.");
+        }
+    }
+}
---
+++ server/PlayItLoud.API/Models/Validators/UniqueOrderNumbersAttribute.cs
@@ -0,0 +1,26 @@
+﻿using PlayItLoud.API.Models.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlayItLoud.API.Models.Validators
+{
+    public class UniqueOrderNumbersAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is ICollection<TrackDTO> tracks)
+            {
+                var duplicateOrderNumbers = tracks.GroupBy(t => t.Order)
+                                                  .Where(g => g.Count() > 1)
+                                                  .Select(g => g.Key)
+                                                  .ToList();
+
+                if (duplicateOrderNumbers.Count != 0)
+                {
+                    return new ValidationResult(ErrorMessage ?? "Track order numbers must be unique.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
