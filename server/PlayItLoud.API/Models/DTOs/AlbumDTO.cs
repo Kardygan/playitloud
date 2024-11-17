f415978ee121d9874e9d96c409821e1671fd963d
Kardygan <k4rdygan@gmail.com>
2024-11-17 17:30:05 +0100

    Update DTOs annotations & Improve clarity

--- server/PlayItLoud.API/Models/DTOs/AlbumDTO.cs
+++ server/PlayItLoud.API/Models/DTOs/AlbumDTO.cs
@@ -1,17 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using PlayItLoud.API.Models.Validators;
+using System.ComponentModel.DataAnnotations;
 
 namespace PlayItLoud.API.Models.DTOs
 {
     public class AlbumDTO
     {
-        [Required(ErrorMessage = "Name is required.")]
-        [StringLength(1000, ErrorMessage = "Name cannot exceed 1000 characters.")]
+        [Required(ErrorMessage = "Album name is required.")]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "Album name must be between 1 and 1000 characters.")]
         public string Name { get; set; }
+
         public DateTime? ReleaseDate { get; set; }
+
         public string? Description { get; set; }
+
         public string? Label { get; set; }
+
+        [NotEmpty(ErrorMessage = "An album must have at least one artist id.")]
         public ICollection<int> ArtistIds { get; set; } = new List<int>();
+
+        [NotEmpty(ErrorMessage = "An album must have at least one genre id.")]
         public ICollection<int> GenreIds { get; set; } = new List<int>();
+
+        [NotEmpty(ErrorMessage = "An album must have at least one track.")]
+        [UniqueOrderNumbers(ErrorMessage = "Track order numbers must be unique.")]
         public ICollection<TrackDTO> Tracks { get; set; } = new List<TrackDTO>();
     }
 }
--- server/PlayItLoud.API/Models/DTOs/TrackDTO.cs
+++ server/PlayItLoud.API/Models/DTOs/TrackDTO.cs
@@ -4,12 +4,14 @@ namespace PlayItLoud.API.Models.DTOs
 {
     public class TrackDTO
     {
-        [Required(ErrorMessage = "Name is required.")]
-        [StringLength(1000, ErrorMessage = "Name cannot exceed 1000 characters.")]
+        [Required(ErrorMessage = "Track name is required.")]
+        [StringLength(1000, MinimumLength = 1, ErrorMessage = "Track name must be between 1 and 1000 characters.")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Order is required.")]
+
+        [Range(1, int.MaxValue, ErrorMessage = "Valid track order is required.")]
         public int Order { get; set; }
-        [Required(ErrorMessage = "Duration is required.")]
-        public TimeSpan Duration { get; set; }
+
+        [Required(ErrorMessage = "Track duration is required.")]
+        public TimeSpan? Duration { get; set; }
     }
 }
