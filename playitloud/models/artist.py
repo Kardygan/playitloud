f654dd28d2f72a6c5e596ca669bae41a0b5f30f9
Kardygan <k4rdygan@gmail.com>
2026-05-19 16:41:59 +0200

    Add Album, Artist and AlbumArtist models

---
+++ playitloud/models/artist.py
@@ -0,0 +1,34 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from typing import TYPE_CHECKING
+from playitloud.models.base import Base
+
+if TYPE_CHECKING:
+    from playitloud.models.album import Album
+
+class Artist(Base):
+    __tablename__ = "artists"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+    )
+    
+    picture_url: Mapped[str | None] = mapped_column(
+        String(255),
+        nullable=True,
+    )
+    
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True,
+    )
+    
+    albums: Mapped[list["Album"]] = relationship(
+        "Album",
+        secondary="album_artists",
+        back_populates="artists",
+    )
