87faa570b3c220832f5a5272f6d134d3c07882a1
Kardygan <k4rdygan@gmail.com>
2026-05-20 14:25:47 +0200

    Add all Supplier models and relationships

---
+++ playitloud/models/supplier.py
@@ -0,0 +1,31 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from playitloud.models.base import Base
+
+if TYPE_CHECKING:
+    from playitloud.models.supplier_offer import SupplierOffer
+    from playitloud.models.supplier_order import SupplierOrder
+
+class Supplier(Base):
+    __tablename__ = "suppliers"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    name: Mapped[str] = mapped_column(
+        String(150),
+        nullable=False,
+        unique=True,
+    )
+    
+    contact_email: Mapped[str | None] = mapped_column(
+        String(255),
+        nullable=True,
+        unique=True,
+    )
+    
+    supplier_offers: Mapped[list["SupplierOffer"]] = relationship(back_populates="supplier")
+    
+    supplier_orders: Mapped[list["SupplierOrder"]] = relationship(back_populates="supplier")
