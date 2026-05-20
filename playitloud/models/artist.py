d4e082c1fcec1cc7e3c51424836a6a9b589e329a
Kardygan <k4rdygan@gmail.com>
2026-05-20 10:14:30 +0200

    Change imports order for consistency

--- playitloud/models/artist.py
+++ playitloud/models/artist.py
@@ -1,7 +1,8 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from typing import TYPE_CHECKING
 from playitloud.models.base import Base
 
 if TYPE_CHECKING:
