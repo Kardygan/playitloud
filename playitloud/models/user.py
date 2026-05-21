64f76f7a7360bb23de4e5d94aefd188c2c385ad0
Kardygan <k4rdygan@gmail.com>
2026-05-21 14:55:59 +0200

    Add timezone parameter to datetime attributes in relevant models

--- playitloud/models/user.py
+++ playitloud/models/user.py
@@ -36,16 +36,21 @@ class User(Base):
     )
     
     created_at: Mapped[datetime] = mapped_column(
-        DateTime,
+        DateTime(timezone=True),
         nullable=False,
         server_default=func.now(),
     )
     
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime,
+        DateTime(timezone=True),
         nullable=False,
         server_default=func.now(),
         onupdate=func.now(),
+    )
+    
+    deleted_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=True,
     )
     
     addresses: Mapped[list["Address"]] = relationship(
