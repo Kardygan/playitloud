53fe1e707bcbf3195bcea1ad1fadddf354970c1b
Kardygan <k4rdygan@gmail.com>
2026-06-08 16:05:00 +0200

    Trim Address and User service tests to core cases

--- tests/services/test_address_service.py
+++ tests/services/test_address_service.py
@@ -1,7 +1,7 @@
 import pytest
 
 from playitloud.repositories import AddressRepository, UserRepository
-from playitloud.schemas import AddressCreate, AddressUpdate, UserCreate
+from playitloud.schemas import AddressCreate, UserCreate
 from playitloud.services import AddressService, UserService
 
 
@@ -40,36 +40,16 @@ def test_create_address(db_session):
     assert address.city == "Paris"
 
 
-def test_create_address_user_not_found(db_session):
-    service = _make_service(db_session)
-
-    with pytest.raises(ValueError, match="User not found"):
-        service.create_address(
-            9999,
-            AddressCreate(street="1 Rue A", city="Paris", postal_code="75001", country_code="FR"),
-        )
-
-
-def test_get_address(db_session):
+def test_create_address_normalizes_country_code(db_session):
     user = _make_user(db_session)
     service = _make_service(db_session)
 
-    created = service.create_address(
+    address = service.create_address(
         user.id,
-        AddressCreate(street="1 Rue A", city="Paris", postal_code="75001", country_code="FR"),
+        AddressCreate(street="1 Rue A", city="Paris", postal_code="75001", country_code="fr"),
     )
 
-    found = service.get_address(user.id, created.id)
-
-    assert found.id == created.id
-
-
-def test_get_address_not_found(db_session):
-    user = _make_user(db_session)
-    service = _make_service(db_session)
-
-    with pytest.raises(ValueError, match="Address not found"):
-        service.get_address(user.id, 9999)
+    assert address.country_code == "FR"
 
 
 def test_get_address_wrong_user(db_session):
@@ -83,49 +63,3 @@ def test_get_address_wrong_user(db_session):
 
     with pytest.raises(ValueError, match="Address not found"):
         service.get_address(user_id=9999, address_id=created.id)
-
-
-def test_get_user_addresses(db_session):
-    user = _make_user(db_session)
-    service = _make_service(db_session)
-
-    service.create_address(user.id, AddressCreate(street="1 Rue A", city="Paris", postal_code="75001", country_code="FR"))
-    service.create_address(user.id, AddressCreate(street="2 Rue B", city="Lyon", postal_code="69001", country_code="FR"))
-
-    addresses = service.get_user_addresses(user.id)
-
-    assert len(addresses) == 2
-
-
-def test_update_address(db_session):
-    user = _make_user(db_session)
-    service = _make_service(db_session)
-
-    created = service.create_address(
-        user.id,
-        AddressCreate(street="Old Street", city="Paris", postal_code="75001", country_code="FR"),
-    )
-
-    updated = service.update_address(
-        user.id,
-        created.id,
-        AddressUpdate(street="New Street", city="Paris", postal_code="75001", country_code="FR"),
-    )
-
-    assert updated.street == "New Street"
-    assert updated.id == created.id
-
-
-def test_delete_address(db_session):
-    user = _make_user(db_session)
-    service = _make_service(db_session)
-
-    created = service.create_address(
-        user.id,
-        AddressCreate(street="1 Rue A", city="Paris", postal_code="75001", country_code="FR"),
-    )
-
-    service.delete_address(user.id, created.id)
-
-    with pytest.raises(ValueError, match="Address not found"):
-        service.get_address(user.id, created.id)
--- tests/services/test_user_service.py
+++ tests/services/test_user_service.py
@@ -1,8 +1,9 @@
 import pytest
 
 from playitloud.repositories import UserRepository
-from playitloud.schemas import UserCreate, UserUpdateInfo
+from playitloud.schemas import UserCreate
 from playitloud.services import UserService
+
 
 def test_create_user(db_session):
     repository = UserRepository(db_session)
@@ -24,6 +25,7 @@ def test_create_user(db_session):
     assert created_user.email == "test@test.com"
     assert created_user.first_name == "Tommy"
 
+
 def test_create_user_duplicate_email(db_session):
     repository = UserRepository(db_session)
 
@@ -43,15 +45,16 @@ def test_create_user_duplicate_email(db_session):
 
     with pytest.raises(ValueError):
         service.create_user(user_create)
-        
+
+
 def test_login_user_success(db_session):
     repository = UserRepository(db_session)
-    
+
     service = UserService(
         session=db_session,
         user_repository=repository,
     )
-    
+
     service.create_user(
         UserCreate(
             email="login@test.com",
@@ -60,135 +63,7 @@ def test_login_user_success(db_session):
             last_name="In",
         )
     )
-        
+
     logged_user = service.login_user("login@test.com", "password")
-    
+
     assert logged_user
-    
-def test_login_user_invalid_email(db_session):
-    repository = UserRepository(db_session)
-
-    service = UserService(
-        session=db_session,
-        user_repository=repository,
-    )
-
-    with pytest.raises(ValueError):
-        service.login_user(
-            email="unknown@test.com",
-            password="secret123",
-        )
-        
-def test_login_user_invalid_password(db_session):
-    repository = UserRepository(db_session)
-
-    service = UserService(
-        session=db_session,
-        user_repository=repository,
-    )
-
-    service.create_user(
-        UserCreate(
-            email="login@test.com",
-            password="correct_password",
-            first_name="Tommy",
-            last_name="Test",
-        )
-    )
-
-    with pytest.raises(ValueError):
-        service.login_user(
-            email="login@test.com",
-            password="wrong_password",
-        )
-
-def test_get_user_by_id(db_session):
-    repository = UserRepository(db_session)
-
-    service = UserService(
-        session=db_session,
-        user_repository=repository,
-    )
-
-    created_user = service.create_user(
-        UserCreate(
-            email="find@test.com",
-            password="password",
-            first_name="Find",
-            last_name="Me",
-        )
-    )
-
-    found_user = service.get_user_by_id(created_user.id)
-
-    assert found_user.id == created_user.id
-    
-def test_get_user_by_email(db_session):
-    repository = UserRepository(db_session)
-
-    service = UserService(
-        session=db_session,
-        user_repository=repository,
-    )
-
-    service.create_user(
-        UserCreate(
-            email="email@test.com",
-            password="password",
-            first_name="Email",
-            last_name="Test",
-        )
-    )
-
-    found_user = service.get_user_by_email("email@test.com")
-
-    assert found_user.email == "email@test.com"
-    
-def test_update_user_info(db_session):
-    repository = UserRepository(db_session)
-
-    service = UserService(
-        session=db_session,
-        user_repository=repository,
-    )
-
-    created_user = service.create_user(
-        UserCreate(
-            email="update@test.com",
-            password="password",
-            first_name="Old",
-            last_name="Name",
-        )
-    )
-
-    updated_user = service.update_user_info(
-        created_user.id,
-        UserUpdateInfo(
-            first_name="New",
-            last_name="Name",
-        ),
-    )
-
-    assert updated_user.first_name == "New"
-    
-def test_delete_user(db_session):
-    repository = UserRepository(db_session)
-
-    service = UserService(
-        session=db_session,
-        user_repository=repository,
-    )
-
-    created_user = service.create_user(
-        UserCreate(
-            email="delete@test.com",
-            password="password",
-            first_name="Delete",
-            last_name="Me",
-        )
-    )
-
-    service.delete_user(created_user.id)
-    deleted_user = repository.get_by_id(created_user.id)
-
-    assert deleted_user is None
